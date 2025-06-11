c5ecbe48513469ba7e2176d5c20042fdb0fbd971
lopeze25 <112291194+lopeze25@users.noreply.github.com>
2025-06-11 15:50:57 -0500

    Update asynPortDriver.cpp

--- asynPortDriver.cpp
+++ asynPortDriver.cpp
@@ -3,6 +3,7 @@
 #include "ScientaAsynDriver.h"
 #include <epicsExport.h>
 #include <stdio.h>
+#include <iostream>
 
 // Scienta DLL function 
 extern "C" uint32_t GDS_ZeroSupplies(void);  
@@ -11,6 +12,7 @@ extern "C" int32_t GDS_GetCurrKineticEnergy(double* value);
 // used in db INP/OUT link
 #define ZeroSuppliesString "SCIENTA_ZERO_SUPPLIES" 
 #define GetCurrentKineticString "SCIENTA_KINETIC_ENERGY_RBV" 
+#define GetInfomation "SCIENTA_INSTRUMENT_INFOMATION" 
 
 // asynPortDriver (asynParamSet *paramSet, const char *portName, int maxAddr, int interfaceMask, int interruptMask, int asynFlags, int autoConnect, int priority, int stackSize) 
 testAsynDriver::testAsynDriver(const char* portName)
@@ -23,6 +25,7 @@ testAsynDriver::testAsynDriver(const char* portName)
 {
     createParam(GetCurrentKineticString, readFloat65, &kineticEnergy); 
     createParam(ZeroSuppliesString, asynParamInt32, &ZeroSupplies);
+    createParam(GetInfomation, readOctet, &string);
 }
 
 
@@ -30,14 +33,27 @@ asynStatus testAsynDriver::writeInt32(asynUser* pasynUser, epicsInt32 value)
 {
     int function = pasynUser->reason;
     asynStatus status = asynSuccess;
-    const char* paramName;
-    const char* functionName = "writeInt32";
-
+//    const char* paramName;
+ //   const char* functionName = "writeInt32";
+(( caget ZeroSupplies : 1))
     if (function == ZeroSupplies && value == 1) {
-          GDS_ZeroSupplies();
-        return status
+          GDS_ZeroSupplies(); 
+    else {
+            status = asynError; 
+            cout << "Error";
+    }    
+    return status
   
 }
+
+asynStatus testAsynDriver::readFloat64(asynUser* pasynUser, epicsInt64, value) {
+    int function = pasynUser->reason;
+
+    
+
+
+}
+
 
 // For the startup st.cmd
 extern "C" {
