3060a5caf4e729be3328926b4fbdccfc2624501c
lopeze25 <112291194+lopeze25@users.noreply.github.com>
2025-06-11 10:35:25 -0500

    Update asynPortDriver.h

--- asynPortDriver.h
+++ asynPortDriver.h
@@ -6,16 +6,14 @@
 
 #include <asynPortDriver.h>
 
-/* This class implements an EPICS asynPortDriver for Scienta using the Mark Rivers testAsynPortDriver style */
+//Based on Mark Rivers testAsynPortDriver style
 class testAsynDriver : public asynPortDriver {
 public:
     testAsynDriver(const char* portName);
-
-    /* Override writeInt32 to support bo/longout record callbacks */
     virtual asynStatus writeInt32(asynUser* pasynUser, epicsInt32 value);
 
 protected:
-    int P_ZeroSupplies;             
+    int ZeroSupplies;             
 };
 
 
