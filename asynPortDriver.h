7fb8c50d81c50c30004e175e4d2ea044d954a6cc
lopeze25 <112291194+lopeze25@users.noreply.github.com>
2025-06-11 11:16:25 -0500

    Update asynPortDriver.h
    Added readFloat64

--- asynPortDriver.h
+++ asynPortDriver.h
@@ -11,9 +11,11 @@ class testAsynDriver : public asynPortDriver {
 public:
     testAsynDriver(const char* portName);
     virtual asynStatus writeInt32(asynUser* pasynUser, epicsInt32 value);
+    virtual asynStatus readFloat64(asynUser* pasynUser, readFloat64 value); 
 
 protected:
-    int ZeroSupplies;             
+    int ZeroSupplies;
+    int kineticEnergy; 
 };
 
 
