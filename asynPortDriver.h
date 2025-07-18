e415c905a20f52cba69a01b3806debe7484a2660
lopeze25 <112291194+lopeze25@users.noreply.github.com>
2025-07-18 06:23:46 -0500

    Update asynPortDriver.h

--- asynPortDriver.h
+++ asynPortDriver.h
@@ -1,21 +1,26 @@
-
-
-
-
 #pragma once
 
 #include <asynPortDriver.h>
+#include <stdint.h>
 
-//Based on Mark Rivers testAsynPortDriver style
-class testAsynDriver : public asynPortDriver {
+class ScientaDriver : public asynPortDriver {
 public:
-    testAsynDriver(const char* portName);
-    virtual asynStatus writeInt32(asynUser* pasynUser, epicsInt32 value);
-    virtual asynStatus readFloat64(asynUser* pasynUser, readFloat64 value); 
-
+    ScientaDriver(const char* portName);
+    virtual asynStatus writeInt32(asynUser* pasynUser, epicsInt32 value) override;
+    virtual asynStatus writeFloat64(asynUser* pasynUser, epicsFloat64 value) override;
+    virtual asynStatus readFloat64(asynUser* pasynUser, epicsFloat64* value) override;
+    virtual asynStatus readOctet(asynUser* pasynUser, char* value, size_t maxChars,
+                                 size_t* nActual, int* eomReason) override;
 protected:
-    int ZeroSupplies;
-    int kineticEnergy; 
-};
-
-
+    int P_KineticEnergy;
+    int P_PassEnergy;
+    int P_LensMode;
+    int P_ElementSet;
+    int P_ElementVoltage;
+    int P_CommStatus;
+    int P_ResetInstrument;
+    int P_LoadInstrumentFile;
+    int P_LoadRunVarFile;
+    int P_ElementSetList;
+    int P_LensModeList;
+    int P_PassEnergyList;
